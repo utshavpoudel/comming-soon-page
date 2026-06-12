8c92da2f8546fc5e58c57fb3ecd04cc5b782354f
utshav88 <poudelutshav333@gmail.com>
2026-06-13 00:18:38 +0545

    commit

---
+++ com.js
@@ -0,0 +1,37 @@
+const targetDate = new Date("June 28, 2026 00:00:00").getTime();
+
+function updateTimer() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24))
+        / (1000 * 60 * 60)
+    );
+
+    const minutes = Math.floor(
+        (distance % (1000 * 60 * 60))
+        / (1000 * 60)
+    );
+
+    const seconds = Math.floor(
+        (distance % (1000 * 60))
+        / 1000
+    );
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if(distance < 0){
+        clearInterval(timer);
+        document.querySelector(".countdown").innerHTML =
+            "<h1>Website is Live!</h1>";
+    }
+}
+
+updateTimer();
+const timer = setInterval(updateTimer, 1000);
